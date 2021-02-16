f84dc627753a3a2b44e27255dce8c12f37a58500
khalidjambi <khalid.jambi@gmail.comm>
2021-02-16 14:21:20 +0700

    percabangan

---
+++ index.js
@@ -0,0 +1,30 @@
+// percabangan if 
+
+// var totalBelanja = prompt("Total Harga Belanja", 0);
+
+// if(totalBelanja > 100000){
+//     document.write("<h2>Selamat Anda dapat hadiah</h2>");
+// }
+
+// document.write("<p>Terima Kasih atas pembayaran nya :)</p>") 
+// => Untuk toko online / aplikasi kasir sangat berguna
+
+
+// var jumlahBelanja = prompt("Total yang harus dibayarkan", 0);
+
+
+// if(jumlahBelanja > 15000){
+//     document.write("<h4>Transaksi belanja anda berhasil</h4>")
+// } else{
+//     document.write("<h4>Transaksi anda gagal, ada pembayaran kurang..</h4>")
+// } => kasir aplikasi
+
+
+
+// var totalPembayaran = prompt("Total Uang", 0);
+
+// if(totalPembayaran == 25000){
+//     document.write('<p>Pembayaran anda lunas</p>');
+// }else{
+//     document.write('<p>Pembayaran anda kurang</p>')
+// } => sistem transaksi
